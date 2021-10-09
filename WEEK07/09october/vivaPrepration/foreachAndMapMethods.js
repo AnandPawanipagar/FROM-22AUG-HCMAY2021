eaa9af0627e8f13cf5c971be503810694cdd2995
AnandPawanipagar <85126312+AnandPawanipagar@users.noreply.github.com>
2021-10-09 19:14:38 +0530

    viva update

--- WEEK07/09october/vivaPrepration/foreachAndMapMethods.js
+++ WEEK07/09october/vivaPrepration/foreachAndMapMethods.js
@@ -1,4 +1,5 @@
 let arr = [1, 2, 3, 4];
+//for every element of the array, foreach method just calls a function but does not return a new array.whereas, map method also calls a function for every element of the array and returns a new array
 let forEach = arr.forEach((e) => {
   return e * e;
 });
